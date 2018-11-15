2a0870389d8917f2f96070aeb1e426c0bdfa6aca
Grace Meilen <gracemeilen@Graces-MacBook-Pro.local>
2018-11-15 14:44:20 -0600

    cant get url to load

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -12,12 +12,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
     @Bean
-    public TimeEntryRepository timeEntryRepository(@Value("${SPRING_DATASOURCE_URL}") String url) {
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl(url);
+    public TimeEntryRepository timeEntryRepository(DataSource dataSource) {
         return new JdbcTimeEntryRepository(dataSource);
     }
 
